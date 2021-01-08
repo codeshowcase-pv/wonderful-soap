157c828e6ea3e1a292a77bad18b55818e4c91531
rasskazov-pv <rasskazov-pv@mosmetro.ru>
2021-01-09 01:21:05 +0300

    add public/js in gitignore, create model,migration,controller for Product

--- public/js/app.js
+++ public/js/app.js
@@ -54970,8 +54970,8 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/rasskazov-pv/programming/wonderful-soap/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/rasskazov-pv/programming/wonderful-soap/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! F:\programming\chudesnoe-mylo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! F:\programming\chudesnoe-mylo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
